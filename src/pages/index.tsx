899673d659df9f09db86c002ab7271369c1e5c7a
4lessandrodev <alessandroadm@live.com>
2022-05-10 01:24:53 -0300

    chore(app): set application initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <h1>Hello there</h1>
+  )
+}
+
+export default Home
