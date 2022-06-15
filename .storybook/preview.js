a26ef140d24d04cb8903ed489997a8b12b70c18c
4lessandrodev <alessandroadm@live.com>
2022-06-15 01:10:05 -0300

    chore(storybook): set context props to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,18 +2,24 @@ import '../src/styles/globals.css';
 import React from "react";
 import theme from '../src/styles/theme';
 import { ThemeProvider } from 'styled-components';
-import AuthProvider from '../src/contexts/auth.provider';
+import AuthProvider from '../src/contexts/auth/auth.provider';
 import { ApolloProvider } from '@apollo/client';
 import { apolloClient } from '../src/lib/apollo'
+import BudgetBoxProvider from '../src/contexts/my-budget-boxes/my-budget-boxes.provider';
+import { CookiesProvider } from 'react-cookie';
 
 export const decorators = [
   (Story) => (
-    <AuthProvider>
-      <ApolloProvider client={apolloClient}>
-      <ThemeProvider theme={theme}>
-        <Story />
-        </ThemeProvider>
-      </ApolloProvider>
-    </AuthProvider>
+    <CookiesProvider>
+      <AuthProvider>
+        <ApolloProvider client={apolloClient}>
+          <BudgetBoxProvider>
+            <ThemeProvider theme={theme}>
+              <Story />
+            </ThemeProvider>
+          </BudgetBoxProvider>
+        </ApolloProvider>
+      </AuthProvider>
+    </CookiesProvider>
   ),
 ];
